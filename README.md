562ce85d654ce3b6f828bfaa92960d329e2beaa2
Holytech <akeemqudus2016@gmail.com>
2023-02-07 13:22:37 +0100

    Extracted the logic of the color picker game as composable

--- README.md
+++ README.md
@@ -27,3 +27,4 @@ npm run dev
 ```sh
 npm run build
 ```
+# altschool-class-of-vue
--- src/components/ColorPickerGame.vue
+++ src/components/ColorPickerGame.vue
@@ -10,23 +10,11 @@
 
 <script>
 import { ref, reactive } from "@vue/reactivity";
+import colorPickerGame from "../composables/color-picker-game";
 
 export default {
   setup() {
-    const colors = ["green", "red", "blue", "purple"];
-    let message = ref("Pick a color...");
-
-    const matchColor = (value) => {
-      // do a random color based on the array index;
-      const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
-
-      if (colors[randomNumber] === value) {
-        message.value = `You win... [answer: ${colors[randomNumber]}]`;
-        return;
-      }
-
-      message.value = `You loose [answer: ${colors[randomNumber]}]`;
-    };
+    const { colors, message, matchColor } = colorPickerGame();
 
     return { colors, message, matchColor };
   },
